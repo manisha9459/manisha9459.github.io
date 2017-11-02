292e29d9e4db89c5fc1536cce7fc82e4740fc22e
manisha9459 <manisha9459@gmail.com>
2017-11-02 21:48:08 +0530

    first  website upload

---
+++ js/Albums.js
@@ -0,0 +1,74 @@
+            
+var gallery = {
+	albums : [
+		{
+			name : "Living room", 
+            thumbnail : "https://i.imgur.com/wYJYTOt.jpg",
+			photos : [
+				{	
+					src : "https://i.imgur.com/wYJYTOt.jpg",
+					title : "Modern Living room ", 
+					description : "Designed with modern interior technique"
+				}, 
+				{	
+					src : "https://i.imgur.com/Q5s3ivS.jpg",
+					title : "Modern Living room 2", 
+					description : "Designed with eblished color's"
+				}, 
+				{	
+					src : "https://i.imgur.com/8M5eNMU.jpg",
+					title : "Modern Living room 3", 
+					description : "Perfect Living room design beside the sea view"
+				}, 
+				{	
+					src : "https://i.imgur.com/kNYa7lA.jpg",
+					title : "old-design Living rom", 
+					description : "An elegant Victoria living room design"
+				} 
+			]
+		},
+		{
+			name : "Bedroom Design's", 
+			thumbnail : "https://i.imgur.com/VxFXhIg.jpg",
+			photos : [
+				{	
+					src : "https://i.imgur.com/VxFXhIg.jpg",
+					title : "Bedroom Design _1", 
+					description : "An simple bedroom design "
+				}, 
+				{	
+					src : "https://i.imgur.com/TlVGTE8.jpg",
+					title : "Bedroom Design _2", 
+					description : "simple interior design with and stylish bed structure"
+				}, 
+				{	
+					src : "https://i.imgur.com/sBb5f8N.jpg",
+					title : "Bedroom Design _3", 
+					description : "an emblished vintage bed design"
+				}  
+			]
+		},
+		{
+			name : "Kitchen room Design's", 
+			thumbnail : "https://i.imgur.com/xeaZvKo.jpg",
+			photos : [
+				{	
+					src : "https://i.imgur.com/xeaZvKo.jpg",
+					title : "design-1", 
+					description : "kitchen design with cabinets,racks,handles."
+				}, 
+				{	
+					src : "https://i.imgur.com/sWIKLOy.jpg",
+					title : "design-2", 
+					description : "an other design with beautiful racks"
+				}, 
+				{	
+					src : "https://i.imgur.com/EjNTn29.jpg",
+					title : "design-3", 
+					description : "an intresting design with simple colors"
+				} 
+			]
+		}
+
+	]
+};
